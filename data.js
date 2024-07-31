cdf7619260f4feac37fb5483b30fffb36bad7374
minsub12 <jejuds987@naver.com>
2024-07-31 11:07:24 +0900

    1

---
+++ data.js
@@ -0,0 +1,471 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "호텔 정문",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "호텔 로비",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.08962256547559,
+          "pitch": 0.11358873452842033,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": 2.47250989244319,
+          "pitch": 0.1663139312159707,
+          "rotation": 5.497787143782138,
+          "target": "4--"
+        },
+        {
+          "yaw": -0.7556776739189388,
+          "pitch": 0.13030268350152063,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "호텔 복도",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09432058580005354,
+          "pitch": 0.12465852609624406,
+          "rotation": 4.71238898038469,
+          "target": "7-------"
+        },
+        {
+          "yaw": -1.5379638926743127,
+          "pitch": 0.6337881333487303,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": 0.38742127779116764,
+          "pitch": 0.10095773960303944,
+          "rotation": 7.853981633974483,
+          "target": "9-------2"
+        },
+        {
+          "yaw": 0.1483226600662686,
+          "pitch": 0.053770037764145684,
+          "rotation": 12.566370614359176,
+          "target": "10--------3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---",
+      "name": "호텔 후문 주차장",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.709384048598112,
+          "pitch": 0.040615220804610885,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "호텔 식당",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5041058890323935,
+          "pitch": 0.4892034277871353,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5------1",
+      "name": "휠체어 접근 가능한 일반 객실 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8905568573885407,
+          "pitch": 0.49387766631859087,
+          "rotation": 6.283185307179586,
+          "target": "7-------"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6443890938174128,
+          "pitch": 0.455191711313514,
+          "title": "침대 높이",
+          "text": "45cm&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "6-------",
+      "name": "휠체어 접근 가능한 일반 객실 화장실 내부",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.1117667349770137,
+          "pitch": 0.5826963314496911,
+          "title": "세면대",
+          "text": "상단 높이 80cm 하단 높이 60cm 깊이 35cm"
+        },
+        {
+          "yaw": -0.5050737243628802,
+          "pitch": -0.13200215352281575,
+          "title": "샤워호스",
+          "text": "샤워 호스 길이 3m"
+        }
+      ]
+    },
+    {
+      "id": "7-------",
+      "name": "휠체어 접근 가능한 일반 객실  현관",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4649365458183716,
+          "pitch": 0.3442768771219811,
+          "rotation": 1.5707963267948966,
+          "target": "6-------"
+        },
+        {
+          "yaw": -3.0378722097616233,
+          "pitch": 0.40105538487636494,
+          "rotation": 0.7853981633974483,
+          "target": "5------1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4806040499122766,
+          "pitch": 0.6972362647810382,
+          "title": "화장실 입구",
+          "text": "단차 0cm 폭 80cm"
+        }
+      ]
+    },
+    {
+      "id": "8-------4",
+      "name": "휠체어 접근 가능한 일반 객실  4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-------2",
+      "name": "휠체어 접근 가능한 일반 객실  2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8466584529493648,
+          "pitch": 0.14538108613403367,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10--------3",
+      "name": "휠체어 접근 가능한 일반 객실   3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0236287033548557,
+          "pitch": 0.3418569843589978,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
